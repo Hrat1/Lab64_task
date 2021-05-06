07b01c830e2569421f83b96bf3aab742235e14d1
Hrat1 <hrat.tahmazyan99@gmail.com>
2021-05-06 13:34:43 +0400

    *Added encrypt functions *Added login with encrypted data code

--- operations/loginOperation.php
+++ operations/loginOperation.php
@@ -1,5 +1,6 @@
 <?php
 include_once "db.php";
+include_once "encrypt.php";
 include_once "session.php";
 publicSession($conn);
 
@@ -11,14 +12,18 @@ if (isset($_POST['log_submit']) && isset($_POST['log_username']) && isset($_POST
     } elseif (strlen($password) < 5) {
         $errorMsg= "Password length is less than 5";
     } else {
-        $signInCommand = "SELECT * FROM admins WHERE username='$username'";
+        $usernameEnc = encrypt($username);
+        $passwordEnc = encrypt($password);
+
+        $signInCommand = "SELECT * FROM admins WHERE username='$usernameEnc'";
         $requestResult = $conn->query($signInCommand);
 
         if ($requestResult->num_rows > 0) {
             $row = mysqli_fetch_assoc($requestResult);
             $passwordRow = $row['password'];
-            if ($passwordRow === $password) {
-                $_SESSION["admin"] = $username;
+
+            if ($passwordRow === $passwordEnc) {
+                $_SESSION["admin"] = $usernameEnc;
                 echo "<script>window.location = window.location.href;</script>";
             }else{
                 $errorMsg = "Please write correct password";
